fd333800e4d87c0e3a2d9fce37589f2417477783
dsavenko <dsavenko@xored.com>
2016-12-28 20:23:07 +0700

    Minor

--- program/aubio/ck-run.sh
+++ program/aubio/ck-run.sh
@@ -5,5 +5,4 @@ NAME="aubio-0.4.3"
 export LD_LIBRARY_PATH="$PWD/$NAME/build/src:$CK_ENV_LIB_RTL_XOPENME_LIB:$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH="$PWD/$NAME/build/src:$CK_ENV_LIB_RTL_XOPENME_LIB:$DYLD_LIBRARY_PATH"
 
-eval $(printenv | awk -F= '{ print "export " $1 }')
 eval $*
